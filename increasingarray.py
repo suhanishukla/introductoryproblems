13df5cbd1a04f8cb6f7647b00dbb00e255150e52
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-04-18 01:23:58 -0700

    Update increasingarray.py

--- increasingarray.py
+++ increasingarray.py
@@ -1,6 +1,11 @@
+#Increasing Array
+#Given an array of n integers. Modify array so that it is increasing, meaning each number is >= the one before. 
+#A move is increasing a number by 1. Print minimum number of moves. 
+
 def increasearray(array): 
-    moves = 0
-    n = len(array)
+    moves = 0               #move is increasing number by 1
+    n = len(array)          #array of n integers
+    
     for i in range(n-1): 
         if array[i] > array[i+1]: 
             x = array[i] - array [i+1]
@@ -9,5 +14,6 @@ def increasearray(array):
             x = 0
     print(moves)
 
+#testingcode
 array = [3, 2, 5, 1, 7]
 increasearray(array)
