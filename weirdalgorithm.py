7561ddf16e2d6f2046ebf321d4b3d42835e54fcb
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-04-18 00:36:02 -0700

    Update weirdalgorithm.py

--- weirdalgorithm.py
+++ weirdalgorithm.py
@@ -4,7 +4,7 @@ def weirdalgorithm(n):
     while n != 1: 
         print(n)
         if n % 2 == 0:              #if n is even divide by 2
-            n = n/2 
+            n = n // 2 
         else: 
             n = n * 3 + 1           #if n is odd then 3n+1
     print(n)
