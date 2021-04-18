d6786b24becfea77365d5e560a63545cbc56a7a7
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-04-17 23:33:36 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+#WeirdAlgorithm
+
+def weirdalgorithm(n): 
+    while n != 1: 
+        print(n)
+        if n % 2 == 0:              #if n is even divide by 2
+            n = n/2 
+        else: 
+            n = n * 3 + 1           #if n is odd then 3n+1
+    print(n)
+  
+weirdalgorithm(3)
+    
+
