380356c6fc821ea250503f1eeb33a0d1190ed5f5
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-04-18 02:16:44 -0700

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,11 +1,17 @@
+#Permutations
+#A permutation of numbers 1,2,...,n is called beautiful if there are no adjacent elements with a difference of 1. 
+#Given n, print a possible beautiful permutation or NO SOLUTIONS if one does not exist. 
+#put all even numbers in ascending order in front of odds in ascending order, does not work for 1,2,and 3
 
 evens = []
 odds = []
 
 def beautification(n): 
+    
     if n <= 3:
         print("NO SOLUTION")
     else: 
+        
         for i in range(1, n+1): 
 
             if i % 2 == 0: 
@@ -16,8 +22,5 @@ def beautification(n):
         beautifulperm = evens + odds
         print(beautifulperm)
 
-beautification(6)
-    
-
-    
-        
+#testcode
+beautification(6) 
