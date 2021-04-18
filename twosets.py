62242cc70483b87a6dc57a4355a3e2f5db2d651e
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-04-18 16:58:13 -0700

    Update twosets.py

--- twosets.py
+++ twosets.py
@@ -1,3 +1,10 @@
+#Two Sets (casework took forever)
+#Divide the numbers 1,2,...,n into two sets of equal sum. Only input is n. 
+#Print YES if possible, NO if not. If YES, print subsets and number of elements in each subset. 
+#So basically this only possible if n(n+1)/2 is divisible by 2, meaning that n = 0 mod 4 or n = 3 mod 4.
+#case n = 0 mod 4: split list in half, take all even indices of first half and odd of second half to create set 1, odd indices of first half and even of second for set 2
+#case n = 3 mod 4: n = 4m - 1, and take m, 2m, and 3m out of list. Split remaining list in half and do same as other case. Add m and 2m to first set, and 3m to second. 
+
 def oddindices(list1):               #function to create list of odd indices of list
     newlist = []
     for index in range(1, len(list1), 2): 
@@ -23,10 +30,12 @@ def equalsets(n):
             middle_index = length//2
             alphafirsthalf = seq[:middle_index]
             alphasechalf = seq[middle_index:]
+            
             #set 1
             evena_firsthalf = evenindices(alphafirsthalf)
             odda_sechalf = oddindices(alphasechalf)
             set1 = evena_firsthalf + odda_sechalf
+            
             #set 2
             odda_firsthalf = oddindices(alphafirsthalf)
             evena_sechalf = evenindices(alphasechalf)
@@ -48,18 +57,23 @@ def equalsets(n):
             middle_index = length//2
             betafirsthalf = seq[:middle_index]
             betasechalf = seq[middle_index:]
+            
             #set 1
             evenb_firsthalf = evenindices(betafirsthalf)
             oddb_sechalf = oddindices(betasechalf)
             set1final = evenb_firsthalf + oddb_sechalf + multiples1
+            
             #set 2
             oddb_firsthalf = oddindices(betafirsthalf)
             evenb_sechalf = evenindices(betasechalf)
             set2final = oddb_firsthalf + evenb_sechalf + multiples2
+            
             #print the final sets
             print(len(set1final))
             print(set1final)
             print(len(set2final))
             print(set2final)
-            
+
+#testingcode
 equalsets(15)
+#equalsets(8)
