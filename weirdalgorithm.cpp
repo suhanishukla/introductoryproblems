4d2fa2a34cb33499253fa5d64af0f5fc81a19283
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-07-03 14:36:35 -0700

    Add files via upload

---
+++ weirdalgorithm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio> 
+
+using namespace std; 
+
+int main() {
+    long long n; 
+    cin >> n; 
+    cout << n << " "; 
+
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2; 
+        } 
+        else {
+            n = 3 * n + 1;   
+        }
+
+        cout << n << " "; 
+    } 
+}
