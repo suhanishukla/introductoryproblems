80cb84366a845cac591f47e005cdb303248dd845
suhanishukla <74700015+suhanishukla@users.noreply.github.com>
2021-07-17 21:31:06 -0700

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+typedef long long ll; 
+
+int main() {
+    ll n; 
+    cin >> n; 
+    ll a, sum = 0; 
+
+    for (ll i=0; i < n-1; i++) {
+        cin >> a; 
+        sum += a; 
+    }
+    cout << (n*(n+1)/2) - sum; 
+}
